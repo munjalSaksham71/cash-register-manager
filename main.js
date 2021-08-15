93ab3db1295ebe34bc63605bf3d599f219638a8f
munjalSaksham71 <sakshammunjal4@gmail.com>
2021-08-15 14:56:39 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,44 @@
+const billAmount = document.getElementById("bill-amount");
+const cashGiven = document.getElementById("cash-given");
+const submit = document.getElementById("submit");
+const message = document.getElementById("error");
+const noOfNotes = document.querySelectorAll(".no-of-notes");
+
+let notes = [2000,500,100,20,10,5,1]
+
+const errorMessage = (msg) => {
+    message.style.display = "block";
+    message.innerText = msg;
+}
+
+const hideMessage = () => {
+    message.style.display = "none";
+}
+
+
+const clickHandler = () => {
+    hideMessage();
+    if (billAmount.value >= 0) {
+        if (cashGiven.value > billAmount.value) {
+            const remainingBalance = cashGiven.value - billAmount.value;
+            notesReturn(remainingBalance);
+        } else if (cashGiven.value == billAmount.value) {
+            errorMessage("No need To give Return")
+        } else {
+            errorMessage("Cash Given Amount is less than Bill Amount")
+        }
+    } else {
+        errorMessage("Enter Valid Amount")
+    }
+
+}
+
+const notesReturn = (remainingBalance) => {
+    for (let i = 0; i < notes.length; i++) {
+        const numberOfNotes = Math.trunc(remainingBalance / notes[i]);
+        remainingBalance = remainingBalance % notes[i];
+        noOfNotes[i].innerText = numberOfNotes;
+    }
+}
+
+submit.addEventListener("click", clickHandler);
